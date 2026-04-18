5863b98cd0c4ba05b033fb0afeb0aa6910cf9ebd
adityaseth <adiseth4519@gmail.com>
2026-04-18 19:41:16 +0530

    Add: combination-target-sum - submission-7

---
+++ Data Structures & Algorithms/combination-target-sum/submission-7.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void solve(List<Integer> arr, List<List<Integer>> ans, int target, int[] nums, int i){
+        if(ans.contains(arr)) return;
+        if(target==0){
+            List<Integer> t=new ArrayList<>();
+            for(int x: arr){
+                t.add(x);
+            }
+            ans.add(t);
+            return;
+        }
+        if(i>=nums.length) return;
+        else{
+            if(nums[i]<=target){
+                arr.add(nums[i]);
+                solve(arr, ans, target-nums[i], nums, i);
+                arr.remove(arr.size()-1);
+            }
+            solve(arr, ans, target, nums, i+1);
+        }
+        
+    }
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<Integer> arr=new ArrayList<>();
+        List<List<Integer>> ans= new ArrayList<>();
+        solve(arr, ans, target, nums, 0);
+        return ans;
+    }
+}
